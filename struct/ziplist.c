f466571a0255fed2ffe06da29e349a445e336ca9
liyoucheng <liyoucheng@danale.com>
2019-10-08 17:29:42 +0800

    noteit

--- struct/ziplist.c
+++ struct/ziplist.c
@@ -1,35 +1,62 @@
 /* The ziplist is a specially encoded dually linked list that is designed
- * to be very memory efficient. It stores both strings and integer values,
+ * to be very memory efficient.
+ * ziplist是为了尽可能地节约内存而设计的特殊编码双端链表
+ * It stores both strings and integer values,
  * where integers are encoded as actual integers instead of a series of
- * characters. It allows push and pop operations on either side of the list
+ * characters.
+ * ziplist可以存储字符串值和整数值，其中整数值被保存为实际的整数，而不是字符数组。
+ *
+ * It allows push and pop operations on either side of the list
  * in O(1) time. However, because every operation requires a reallocation of
  * the memory used by the ziplist, the actual complexity is related to the
  * amount of memory used by the ziplist.
- *
+ * ziplist允许在列表的两端进行O（1）复杂度的push和pop操作。
+ * 但是，因为这些操作都是需要对整个ziplist进行内存重分配，
+ * 所以实际的复杂度和ziplist占用的内存大小有关。
  * ----------------------------------------------------------------------------
  *
  * ZIPLIST OVERALL LAYOUT:
+ * ziplist的整体布局：
+ *
  * The general layout of the ziplist is as follows:
+ * 以下是ziplist的一般布局:
  * <zlbytes><zltail><zllen><entry><entry><zlend>
  *
  * <zlbytes> is an unsigned integer to hold the number of bytes that the
  * ziplist occupies. This value needs to be stored to be able to resize the
  * entire structure without the need to traverse it first.
+ * <zlbytes>是一个无符号整数，保存着ziplist使用的内存数据。
+ *
+ * 通过这个值，程序可以直接对ziplist的内存大小进行调整，
+ * 而无须为了计算ziplist的内存大小而遍历整个列表。
  *
  * <zltail> is the offset to the last entry in the list. This allows a pop
  * operation on the far side of the list without the need for full traversal.
  *
+ * <zltail>保存着到达列表中最后一个节点的偏移量。
+ *
+ * 这个偏移量使得对表尾的pop操作可以在无须遍历整个列表的情况下进行。
+ *
  * <zllen> is the number of entries.When this value is larger than 2**16-2,
  * we need to traverse the entire list to know how many items it holds.
+ * 
+ * <zllen>保存着列表中的节点数量
+ * 程序需要遍历整个列表才能知道列表包含了多少个节点
  *
  * <zlend> is a single byte special value, equal to 255, which indicates the
  * end of the list.
  *
+ * <zlend>的长度为1字节，值为255，标识列表的末尾。
+ * 
  * ZIPLIST ENTRIES:
  * Every entry in the ziplist is prefixed by a header that contains two pieces
  * of information. First, the length of the previous entry is stored to be
  * able to traverse the list from back to front. Second, the encoding with an
  * optional string length of the entry itself is stored.
+ * 每个ziplist节点的前面都带有一个header，这个header包含两部分信息：
+ * 1）前置节点的长度，在程序从后向前遍历时使用；
+ * 2）当前节点所保存的值的类型和长度。
+ *
  *
  * The length of the previous entry is encoded in the following way:
  * If this length is smaller than 254 bytes, it will only consume a single
@@ -38,6 +65,15 @@
  * indicate a larger value is following. The remaining 4 bytes take the
  * length of the previous entry as value.
  *
+ * 编码前置节点的长度的方法如下：
+ *
+ * 1）如果前置节点的长度小于254字节，那么程序将使用1个字节来保存这个长度值
+ *
+ * 2）如果前置节点的长度大于等于254字节，那么程序将使用5个字节来保存这个长度值：
+ *      a）第1个字节的值将被设为0xfe，用于标识这是一个5字节长的长度值
+ *      b）之后的4个字节则用于保存前置节点的实际长度。
+ *
+ *
  * The other header field of the entry itself depends on the contents of the
  * entry. When the entry is a string, the first 2 bits of this header will hold
  * the type of encoding used to store the length of the string, followed by the
@@ -45,6 +81,12 @@
  * are both set to 1. The following 2 bits are used to specify what kind of
  * integer will be stored after this header. An overview of the different
  * types and encodings is as follows:
+ * 
+ * header另一部分的内容和节点所保存的值有关
+ *
+ * 1）如果节点保存的是字符串值，
+ *    那么这部分header的头两个位保存编码字符串长度所使用的类型，
+ *    而之后跟着的内容则是字符串的实际长度。
  *
  * |00pppppp| - 1 byte
  *      String value with length less than or equal to 63 bytes (6 bits).
